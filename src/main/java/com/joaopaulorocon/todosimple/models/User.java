e25a3dfeee64fdf2f8d25e112197bde0366faa63
João Paulo Rocon <joaopaulorocon@gmail.com>
2023-04-11 15:23:16 -0300

    Create User Model

--- src/main/java/com/joaopaulorocon/todosimple/models/User.java
+++ src/main/java/com/joaopaulorocon/todosimple/models/User.java
@@ -1,6 +1,8 @@
 package com.joaopaulorocon.todosimple.models;
 
 
+import java.util.Objects;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -12,6 +14,7 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import org.hibernate.sql.Update;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 @Entity
 @Table(name = User.TABLE_NAME)
@@ -35,9 +38,69 @@ public class User {
     @Size(groups = CreateUser.class, min = 2, max = 100)
     private String userName;
 
+    
     @Column(name = "password", length = 60, nullable = false)
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     @NotNull(groups = {CreateUser.class, UpdateUser.class})
     @NotEmpty(groups = {CreateUser.class, UpdateUser.class})
     @Size(groups = {CreateUser.class, Update.class},min = 8, max = 60)
     private String password;
+
+    //private List<Task> tasks = new ArrayList<Task>();
+
+
+    public User() {
+    }
+
+
+
+    public User(Long id, String userName, String password) {
+        this.id = id;
+        this.userName = userName;
+        this.password = password;
+    }
+
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return this.userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this)
+            return true;
+        if (!(obj instanceof User)) {
+            return false;
+        }
+        User user = (User) obj;
+        return Objects.equals(id, user.id) && Objects.equals(userName, user.userName) && Objects.equals(password, user.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, userName, password);
+    }
+
+
 }
